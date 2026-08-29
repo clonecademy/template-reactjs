a1612024e0b439b9bd7b19b71e37745c7c26f9a8
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-08-29 16:00:01 -0700

    feat: init shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@
 // PATH: /src/routes/index.tsx
 
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
 	component: Home,
@@ -9,6 +10,8 @@ export const Route = createFileRoute("/")({
 
 function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center grow">Home</div>
+		<div className="flex flex-col items-center justify-center grow">
+			<Button>Home</Button>
+		</div>
 	);
 }
